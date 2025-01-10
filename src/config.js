5e9915028ce728e7a5066a2e12c090d59a431fb3
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-10 15:59:37 -0600

    chore: wip

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import path from 'node:path';
 dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 
 export const appConfig = {
-  env: process.env.NODE_ENV,
+  env: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT || 80),
 }
 
--- src/db/migrations/20250110205755_create_tables.js
+++ src/db/migrations/20250110205755_create_tables.js
@@ -7,8 +7,20 @@ export async function up(knex) {
 		table.string('sid', 255).primary().notNullable();
 		table.json('sess').notNullable();
 		table.timestamp('expired').notNullable();
-
 		table.index(['expired'], 'sessions_expired_index');
+	});
+
+	await knex.schema.createTable('users', (table) => {
+		table.increments('id').primary();
+		table.string('username').unique().notNullable();
+		table.string('email').unique().notNullable();
+		table.boolean('is_admin').defaultTo(false);
+		table.string('api_key').unique().nullable();
+		table.integer('api_key_version').defaultTo(0).notNullable();
+		table.timestamp('api_key_created_at').nullable();
+		table.timestamps(true, true);
+		table.index('api_key');
+		table.index(['email', 'is_admin', 'username']);
 	});
 }
 
@@ -17,7 +29,7 @@ export async function up(knex) {
  * @returns { Promise<void> }
  */
 export async function down(knex) {
-  for (const table of ['sessions']) {
+  for (const table of ['users','sessions']) {
 		await knex.schema.dropTableIfExists(table);
 	}
 }
