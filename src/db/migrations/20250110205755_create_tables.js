3452898192b7c6b2ed4acf080958a8134dbca1e8
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-10 17:19:56 -0600

    chore: wip

--- src/db/migrations/20250110205755_create_tables.js
+++ src/db/migrations/20250110205755_create_tables.js
@@ -2,36 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function up(knex) {
-	await knex.schema.createTable('sessions', (table) => {
+export function up(knex) {
+	return knex.schema.createTable('sessions', (table) => {
 		table.string('sid', 255).primary().notNullable();
 		table.json('sess').notNullable();
 		table.timestamp('expired').notNullable();
 
 		table.index(['expired'], 'sessions_expired_index');
-	});
+	})
+		.createTable('users', (table) => {
+			table.increments('id').primary();
+			table.string('username').unique().notNullable();
+			table.string('email').unique().notNullable();
+			table.boolean('is_admin').defaultTo(false);
+			table.string('api_key').unique().nullable();
+			table.integer('api_key_version').defaultTo(0).notNullable();
+			table.timestamp('api_key_created_at').nullable();
+			table.timestamps(true, true);
 
-	await knex.schema.createTable('users', (table) => {
-		table.increments('id').primary();
-		table.string('username').unique().notNullable();
-		table.string('email').unique().notNullable();
-		table.boolean('is_admin').defaultTo(false);
-		table.string('api_key').unique().nullable();
-		table.integer('api_key_version').defaultTo(0).notNullable();
-		table.timestamp('api_key_created_at').nullable();
-		table.timestamps(true, true);
-
-		table.index('api_key');
-		table.index(['email', 'is_admin', 'username']);
-	});
+			table.index('api_key');
+			table.index(['email', 'is_admin', 'username']);
+		});
 }
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function down(knex) {
-  for (const table of ['users','sessions']) {
-		await knex.schema.dropTableIfExists(table);
-	}
+export function down(knex) {
+	return knex.schema.dropTableIfExists('users')
+		.dropTableIfExists('session');
 }
