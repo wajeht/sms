c57638f79065b18e71f3cb999f735d278b190eb1
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-10 23:15:04 -0600

    feat: Add TypeScript configuration and new page render handlers

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express'
 
+// GET /healthz
 export function getHealthzHandler (req: Request, res: Response) {
    res.status(200).send('ok');
 }
 
+// GET /
 export function getHomepageHandler(req: Request, res: Response) {
   return res.render('home.html');
+}
+
+// GET /privacy-policy
+export function getPrivacyPolicyPageHandler(req: Request, res: Response) {
+  return res.render('privacy-policy.html');
+}
+
+// GET /terms-of-service
+export function getTermsOfServicePageHandler(req: Request, res: Response) {
+  return res.render('terms-of-service.html');
 }
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getHealthzHandler, getHomepageHandler } from './handler'
+import { getHealthzHandler, getHomepageHandler, getPrivacyPolicyPageHandler, getTermsOfServicePageHandler } from './handler'
 
 const router = express.Router();
 
 router.get('/', getHomepageHandler)
 
 router.get('/healthz', getHealthzHandler)
+
+router.get('/privacy-policy', getPrivacyPolicyPageHandler)
+
+router.get('/terms-of-service', getTermsOfServicePageHandler)
 
 export { router };
