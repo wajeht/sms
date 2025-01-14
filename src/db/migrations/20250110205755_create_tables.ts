faa2edac35ff577570e5d6ee9f8ee17c6a35e314
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-13 23:08:03 -0600

    feat(db): Add migration for creating 'carriers' and 'carrier_emails' tables

--- src/db/migrations/20250110205755_create_tables.ts
+++ src/db/migrations/20250110205755_create_tables.ts
@@ -18,10 +18,30 @@ export async function up(knex: Knex): Promise<void> {
 
 		table.index(['email', 'is_admin', 'username']);
 	});
+
+	await knex.schema.createTable('carriers', (table) => {
+		table.increments('id').primary();
+		table.string('name').notNullable();
+		table.string('category', 1).notNullable();
+		table.timestamps(true, true);
+
+		table.index('name');
+		table.index('category');
+});
+
+await knex.schema.createTable('carrier_emails', (table) => {
+	table.increments('id').primary();
+		table.string('carrier_id', 255).notNullable();
+		table.string('email').notNullable();
+		table.foreign('carrier_id').references('id').inTable('carriers').onDelete('CASCADE');
+
+		table.index('carrier_id');
+		table.index('email');
+});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	for (const table of ['users', 'sessions']) {
+	for (const table of ['carrier_emails', 'carriers', 'users', 'sessions']) {
 		await knex.schema.dropTableIfExists(table);
 	}
 }
