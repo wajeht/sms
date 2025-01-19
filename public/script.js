6ee572c6b03d37f8e6988f5f7914a0b913e532f6
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-19 02:25:06 -0600

    style: Update dark mode styles for theme toggle button and components

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-console.log('hello world!');
+function toggleTheme() {
+	if (document.documentElement.classList.contains('dark')) {
+		document.documentElement.classList.remove('dark');
+		localStorage.theme = 'light';
+	} else {
+		document.documentElement.classList.add('dark');
+		localStorage.theme = 'dark';
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	important: true,
+	darkMode: ['selector'],
 	content: ['./src/views/**/*.{vue,js,ts,jsx,tsx,html,ejs}'],
 	theme: {
 		extend: {},
