40a2c4624f664901e3bbb47abd780600eaf6137d
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-26 22:08:30 -0600

    feat(config): Add email configuration constants to the application

--- src/config.ts
+++ src/config.ts
@@ -9,15 +9,25 @@ export const appConfig = {
 	appUrl: process.env.APP_URL || 'localhost',
 	env: (process.env.NODE_ENV as Env) || 'development',
 	adminEmail: process.env.APP_ADMIN_EMAIL || '',
-};
+} as const;
 
 export const scrapeConfig = {
 	carrierWebsiteUrlOne: process.env.CARRIER_WEBSITE_URL_ONE || '',
 	carrierWebsiteUrlTwo: process.env.CARRIER_WEBSITE_URL_TWO || '',
 	carrierWebsiteUrlThree: process.env.CARRIER_WEBSITE_URL_THREE || '',
-};
+} as const;
 
 export const sessionConfig = {
 	secret: process.env.SESSION_SECRET || 'sms',
 	domain: process.env.SESSION_DOMAIN || 'localhost',
-};
+} as const;
+
+export const emailConfig = {
+	host: process.env.EMAIL_HOST || '',
+	port: parseInt(process.env.EMAIL_PORT || '0', 10),
+	alias: process.env.EMAIL_ALIAS || '',
+	auth: {
+		user: process.env.EMAIL_AUTH_EMAIL || '',
+		pass: process.env.EMAIL_AUTH_PASS || '',
+	},
+} as const;
